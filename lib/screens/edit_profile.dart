94e4ff9209bd8fb044d4fc185ad7ffad3682bf88
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2022-01-19 08:51:50 +0500

    Update edit_profile.dart

--- lib/screens/edit_profile.dart
+++ lib/screens/edit_profile.dart
@@ -1,15 +1,12 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import "package:flutter/material.dart";
 import 'package:sketch_to_real/config/collection_names.dart';
 import 'package:sketch_to_real/constants.dart';
 import 'package:sketch_to_real/models/user_model.dart';
-import 'package:sketch_to_real/tools/loading.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
 
 class EditProfile extends StatefulWidget {
   final String currentUserID;
